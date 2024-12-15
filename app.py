9370a0295eca820ae97964b7a6b7bff6e3f7f774
Zahraa-sy <85197163+Zahraa-sy@users.noreply.github.com>
2024-12-15 15:56:10 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, requests,request
+from flask import Flask,request
 from config import TOKEN
 from bot import bot, telebot
 
@@ -14,14 +14,6 @@ def webhook():
     return '', 200
 
 if __name__ == '__main__':
-    # Setting the webhook URL before running the Flask app
-    webhook_url = f"https://mybot-bml5.onrender.com/{TOKEN}"  # Replace with your actual domain
-    set_webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={webhook_url}"
-    response = requests.get(set_webhook_url)
-    if response.status_code == 200:
-        print("Webhook has been set successfully!")
-    else:
-        print("Failed to set webhook. Please check your API token and URL.")
-    
+
     # Running the Flask app
     app.run(host='0.0.0.0', port=5000)
