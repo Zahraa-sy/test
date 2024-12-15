18a12ec363ebcfb6ecf4e68699b7fe110cbc6501
Zahraa-sy <85197163+Zahraa-sy@users.noreply.github.com>
2024-12-15 21:19:01 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import time
 import telebot
 import requests
 from telebot import types
@@ -11,22 +12,34 @@ bot = telebot.TeleBot(TOKEN)
 
 # Function to load JSON data from a URL
 def load_json_data(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error loading data from {url}: {e}")
-        return {}
+    max_attempts = 5
+    attempt = 0
+    while attempt < max_attempts:
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Attempt {attempt+1} failed: {e}")
+            attempt += 1
+            time.sleep(1) # انتظر ثانية قبل المحاولة التالية
+    print("Failed to load JSON data after multiple attempts.")
+    return None
+
 
 # روابط JSON
 url_telegram_users = "https://script.google.com/macros/s/AKfycbwMF9ajqKdnX7m3caoympN5NxYc3RrSg7VJ5cbuDxQvIrlv9x575LLeitFkrGnN0g4ZiQ/exec"
-url_extract_messages = "https://script.google.com/macros/s/AKfycbxC1w62816BdFDUqtqE-vkiWUFAYfLxQBgFOLjtpfJlY9XJM6o1SngacEbj21PmDpbMNA/exec?action=importLatestMessagesFromToday"
-url_messages = "https://script.google.com/macros/s/AKfycbxC1w62816BdFDUqtqE-vkiWUFAYfLxQBgFOLjtpfJlY9XJM6o1SngacEbj21PmDpbMNA/exec?action=getFirstTenMessages"
+#url_allowed_names = "https://script.google.com/macros/s/AKfycbys-RD07ry6FbYD3vy91Dhr6Zb3xQdZwBjSRuNk7FLhVO72c8K7z13U3C7iVMQkN03wDQ/exec"                        
+url_extract_messages = "https://script.google.com/macros/s/AKfycbzl22XF2Wr8np3Fe2a41aIsUJE4wytdyhPAQ4CQGS0oH--Zt_nfo92eIAgcZsx4W6AzRw/exec?action=importLatestMessagesFromToday"
+url_messages = "https://script.google.com/macros/s/AKfycbzl22XF2Wr8np3Fe2a41aIsUJE4wytdyhPAQ4CQGS0oH--Zt_nfo92eIAgcZsx4W6AzRw/exec?action=getFirstTenMessages"
 
 # تحميل أسماء مستخدمي تيليجرام
 data_telegram_users = load_json_data(url_telegram_users)
 telegram_users = [item['username'] for item in data_telegram_users.get('telegram_users', [])]
+
+# تحميل الأسماء المصرح بها
+#data_allowed_names = load_json_data(url_allowed_names)
+#allowed_names_accounts = data_allowed_names.get('allowed_names', [])
 
 # تحميل الأسماء المصرح بها
 def load_json_data_from_file(file_path):
@@ -51,8 +64,10 @@ def start_message(message):
 def process_name(message):
     global user_name, user_account
     user_name = message.from_user.username.strip()
-    user_account = message.text.strip()    
-    # تحقق مما إذا كان الحساب موجودًا    
+    user_account = message.text.strip()
+    
+    # تحقق مما إذا كان الحساب موجودًا
+    
     for account in allowed_names_accounts:
         if account['username'].lower() == user_name.lower() and user_account in account['accounts']:
             bot.send_message(message.chat.id, f"شكرًا، {user_name}")
@@ -65,7 +80,9 @@ def process_name(message):
     # إذا لم يكن الحساب صالحًا، اطلب إعادة المحاولة
     bot.send_message(message.chat.id, "الحساب ليس لك. يرجى إدخال اسم حساب جديد.")
     bot.send_message(message.chat.id, "يرجى استخدام الأمر /start لإعادة البدء.")
-#-------
+    #  المستخدم يستخدم الأمر /start لإعادة البدء
+
+#----------------
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     user_name = message.from_user.username.strip()
@@ -94,9 +111,11 @@ def handle_text(message):
         'رمز الوصول المؤقت من Netflix'
     ]:
         data_messages = load_json_data(url_messages)
+        if data_messages is None:
+            bot.send_message(message.chat.id, "فشل في تحميل البيانات JSON.")
+            return
         messages_data = data_messages.get('messages', [])
-        user_account = message.text.strip()
-        
+        user_account = message.text.strip()        
         user_messages = [item for item in messages_data if item['to'] == user_account]
         
         if user_messages:
@@ -108,6 +127,7 @@ def handle_text(message):
                 bot.send_message(message.chat.id, "ليس لديك رسالة بالعنوان المطلوب.")
         else:
             bot.send_message(message.chat.id, "ليس لديك رسائل.")
+
 
 # Flask app
 app = Flask(__name__)
